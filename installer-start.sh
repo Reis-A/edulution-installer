618fc00be7da379a24e363b481c9224a2daf4452
Reis-A <114596917+Reis-A@users.noreply.github.com>
2025-11-18 15:27:54 +0100

    added switch branch

--- installer-start.sh
+++ installer-start.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+cd /root
 git clone https://github.com/Reis-A/edulution-installer
+cd edulution-installer
+git checkout "nov46"
+cd /root
 cp edulution-installer/public/installer . 
 bash installer
